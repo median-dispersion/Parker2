b55ca4eae1bc1f7e3bfe236755e02c8d5bd98d21
median-dispersion <mediandispersion@gmail.com>
2026-04-07 18:18:12 +0200

    Added Miller-Rabin primality test

--- Client/Search/Source/PrimeDecomposition.cpp
+++ Client/Search/Source/PrimeDecomposition.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 // ================================================================================================
 // Get all prime factors of e² using trial division
+// Maximum input value e = (2³²-1)²
 // ================================================================================================
 vector<pair<uint_fast64_t, uint_fast64_t>> squaredTrialDivision1mod4(uint_fast64_t e) {
 
     // This function will get all prime factors and their exponents for the squared value of e i.e. e² using trial division
     // But only if the prime factor itself is primeFactor ≡ 1 (mod 4) else no prime factors will be returned
+    // This function only works for values of e = 1 (mod 4)
 
     // List of all prime factors
     vector<pair<uint_fast64_t, uint_fast64_t>> primeFactors;
@@ -71,5 +73,164 @@ vector<pair<uint_fast64_t, uint_fast64_t>> squaredTrialDivision1mod4(uint_fast64
 
     // Return all prime factors
     return primeFactors;
+
+}
+
+// ================================================================================================
+// Modular multiplication using __uint128_t to allow multiplication of two 64-bit integers
+// Maximum input values = 2⁶⁴-1
+// ================================================================================================
+uint_fast64_t modularMultiplication(
+    uint_fast64_t factor1,
+    uint_fast64_t factor2,
+    uint_fast64_t modulus
+) {
+
+    return static_cast<__uint128_t>(factor1) * factor2 % modulus;
+
+}
+
+// ================================================================================================
+// Modular exponentiation (https://en.wikipedia.org/wiki/Modular_exponentiation#Pseudocode)
+// Maximum input values = 2⁶⁴-1
+// ================================================================================================
+uint_fast64_t modularExponentiation(
+    uint_fast64_t base,
+    uint_fast64_t exponent,
+    uint_fast64_t modulus
+) {
+
+    // Initialize result to 1
+    uint_fast64_t result = 1;
+
+    // Reduce base to prevent overflows
+    base %= modulus;
+
+    // Loop until the exponent becomes zero
+    while (exponent > 0) {
+
+        // Check if the exponent is odd using a bitwise AND
+        if (exponent & 1) {
+
+            // Multiply the result by the base
+            result = modularMultiplication(result, base, modulus);
+
+        }
+
+        // Divide the exponent by 2 using a right bit shift
+        exponent >>= 1;
+
+        // Square the base
+        base = modularMultiplication(base, base, modulus);
+
+    }
+
+    // Return the result
+    return result;
+
+}
+
+// ================================================================================================
+// Miller-Rabin Primality test (https://cp-algorithms.com/algebra/primality_tests.html)
+// Maximum input value n = 2⁶⁴-1
+// ================================================================================================
+bool millerRabinPrimalityTest(uint_fast64_t n) {
+
+    // Check if the number is less than 2
+    if (n < 2) {
+
+        // No number less than 2 can be prime
+        // Return not prime
+        return false;
+
+    }
+
+    // Check small primes directly witch is faster than using Miller-Rabin
+    // Values from https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases
+    for (uint_fast64_t p : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+
+        // Check if the number is divisible by the prime
+        if (n % p == 0) {
+
+            // Return true only if the number itself is the prime
+            return n == p;
+
+        }
+
+    }
+
+    // Initialize d and s for d*2^s
+    uint_fast64_t d = n - 1;
+    uint_fast64_t s = 0;
+
+    // Loop as long as d is even using a bitwise AND check
+    // This will write n-1 as d*2^2 by factoring out powers of 2
+    while ((d & 1) == 0) {
+
+        // Divide d by 2 using a right bit shift
+        d >>= 1;
+
+        // Increase s
+        s++;
+
+    }
+
+    // Deterministic set of bases that works for all 64-bit integers
+    // Values from https://miller-rabin.appspot.com/
+    for (uint_fast64_t a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
+
+        // Check if base is a multiple of n
+        if (a % n == 0) {
+
+            // Skip this base
+            continue;
+
+        }
+
+        // Get x
+        uint_fast64_t x = modularExponentiation(a, d, n);
+
+        // Check if x is 1 or n - 1
+        if (x == 1 || x == n - 1) {
+
+            // Skip this base
+            continue;
+
+        }
+
+        // Flag for checking if the number is a composite
+        bool composite = true;
+
+        // Repeat squaring up to s-1 times
+        for (uint_fast64_t r = 1; r < s; r++) {
+
+            // Square x modulus n
+            x = modularMultiplication(x, x, n);
+
+            // Check if x is n-1
+            if (x == n - 1) {
+
+                // Set the composite flag to false
+                composite = false;
+
+                // Stop the squaring loop immediately
+                break;
+
+            }
+
+        }
+
+        // Check if the number is a composite
+        if (composite) {
+
+            // Return that the number is not a prime
+            return false;
+
+        }
+
+    }
+
+    // Return that the number is a prime
+    return true;
 
 }
